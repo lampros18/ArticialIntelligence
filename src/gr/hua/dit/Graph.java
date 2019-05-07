686838b4935f374d68dee7a5d36113e89f1c5436
lampros18 <44374754+lampros18@users.noreply.github.com>
2019-05-07 21:55:05 +0300

    Minor Refactoring

--- src/gr/hua/dit/Graph.java
+++ src/gr/hua/dit/Graph.java
@@ -59,22 +59,6 @@ class Graph {
         DFSUtil(start, target, visited);
     }
 
-//    Best-First-Search(Grah g, Node start)
-//    1) Create an empty PriorityQueue
-//    PriorityQueue pq;
-//    2) Insert "start" in pq.
-//            pq.insert(start)
-//    3) Until PriorityQueue is empty
-//    u = PriorityQueue.DeleteMin
-//    If u is the goal
-//            Exit
-//    Else
-//    Foreach neighbor v of u
-//    If v "Unvisited"
-//      Mark v "Visited"
-//       pq.insert(v)
-//    Mark u "Examined"
-//    End procedure
 
     void bestFirstSearch(Node start, Node target){
 
